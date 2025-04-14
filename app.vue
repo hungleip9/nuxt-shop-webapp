ae2541878aedabaffe75838b6146f32631d9820c
hungleip9@gmail.com <hungleip9@gmail.com>
2025-04-14 10:09:42 +0700

    updated

--- app.vue
+++ app.vue
@@ -1,7 +1,17 @@
+<script setup lang="ts">
+if (import.meta.server) {
+  useSeoMeta({
+    title: 'NuxtShop Coffee',
+    ogTitle: 'NuxtShop Coffee',
+    description: 'Cà phê ngon số 1 Việt Name.',
+    ogDescription: 'Cà phê ngon số 1 Việt Name.',
+  })
+}
+</script>
 <template>
   <div>
-    <div>aa</div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <el-button @click="ElMessage('hello')">button</el-button>
+    <ElButton :icon="ElIconEditPen" type="success">button</ElButton>
+    <LazyElButton type="warning">lazy button</LazyElButton>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,32 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-
+  experimental: { sharedPrerenderData: true },
+  nitro: { preset: 'node-server', compressPublicAssets: true },
+  components: true,
   modules: [
     '@nuxt/eslint',
     '@nuxt/image',
     '@nuxt/icon',
-    '@nuxt/fonts'
-  ]
+    '@nuxt/fonts',
+    '@element-plus/nuxt'
+  ],
+  ssr: true,
+  routeRules: {
+
+  },
+  elementPlus: {
+    // Các tùy chọn cấu hình (nếu cần)
+  },
+  app: {
+    head: {
+      title: 'Nuxtshop Coffee', // default fallback title
+      meta: [
+        { name: 'description', content: 'Cà phê ngon số 1 Việt Nam.' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ]
+    }
+  }
 })
